36ca8942c6abc094687606eb5d64188ab10780ca
Panhakoeun <panha.koeun@student.passerellesnumeriques.org>
2025-11-07 12:11:14 +0700

    fix error cannot run already

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from face_attendance.app import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # Using a different port to avoid conflicts
+    app.run(
+        debug=True,  # Enabling debug mode temporarily for testing
+        host='localhost',  # Binding specifically to localhost
+        port=5001  # Using a different port
+    )
